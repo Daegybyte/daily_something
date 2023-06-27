3da067a0db79bb0cecc210cfe4626e08905b32f4
Diego <lucy@lucy.local>
2023-06-27 17:07:56 -0600

    a faster way to determine an odd or even number using bitwise operations with asserts, now with a snazzy status bar

--- fast_odd_even.py
+++ fast_odd_even.py
@@ -1,20 +1,21 @@
 # !/usr/bin/env python3
-
+from tqdm import tqdm  
+from time import sleep
 class Sandbox:
   def doit():
-    nums = [num for num in range(-20,20)]
+    nums = range(-100_000_000,100_000_000)
+    progress_bar = tqdm(nums, desc="checking odds and evens", total=len(nums))
+
     for num in nums:
-      if num == 0:
-        # print(f"{num} is special")
-        continue
       if num & 1:
         assert(num % 2 == 1)
         # print(f"{num} is odd")
       else:
         assert(num % 2 == 0)
         # print(f"{num} is even")
-    
-    print(f"finished checking {len(nums)} numbers")
+      progress_bar.update()
+  
+    progress_bar.close()
 
 if __name__ == "__main__":
   s = Sandbox
