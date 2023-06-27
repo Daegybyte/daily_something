5a1f9ebc9a7d0439e810a13692ea8b80b646176f
Diego <lucy@lucy.local>
2023-06-27 15:10:00 -0600

    a faster way to determine an odd or even number using bitwise operations with asserts

--- fast_odd_even.py
+++ fast_odd_even.py
@@ -2,15 +2,19 @@
 
 class Sandbox:
   def doit():
-    nums = [num for num in range(0,1_000)]
+    nums = [num for num in range(-20,20)]
     for num in nums:
-      if num is 0:
-        print(f"{num} is special")
+      if num == 0:
+        # print(f"{num} is special")
         continue
       if num & 1:
-        print(f"{num} is odd")
+        assert(num % 2 == 1)
+        # print(f"{num} is odd")
       else:
-        print(f"{num} is even")
+        assert(num % 2 == 0)
+        # print(f"{num} is even")
+    
+    print(f"finished checking {len(nums)} numbers")
 
 if __name__ == "__main__":
   s = Sandbox
