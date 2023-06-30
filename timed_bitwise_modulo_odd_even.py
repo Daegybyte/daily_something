c1a4d399bdfe57f9c88f8869f620b5b8c76e3057
Diego <lucy@lucy.local>
2023-06-30 15:37:01 -0600

    refactored code to use timeit in place of time as this is considered best practices. Results from timeing commented at the bottom in main

--- timed_bitwise_modulo_odd_even.py
+++ timed_bitwise_modulo_odd_even.py
@@ -1,39 +1,29 @@
 # !/usr/bin/env python3
-import time
+import timeit
 import statistics
 
 class Timed_Odd_Even:
     range_number = 100_000_000
     nums = range(-range_number, range_number)
     
-    def __init__(self, nums) -> None:
-       self.nums = nums
+    def __init__(cls, nums) -> None:
+       cls.nums = nums
     
     @classmethod
-    def bit_odd_even(self):
-        T = []
-        for num in self.nums:
-            start = time.time()
-            if num & 1:
-                pass
-            else:
-                pass
-            end = time.time()
-            T.append(end - start)
-        return statistics.mean(T)
+    def bit_odd_even(cls):
+        times = []
+        for num in cls.nums:
+            t = timeit.timeit(lambda: num & 1 != 0, number=1) 
+            times.append(t)
+        return statistics.mean(times)
     
     @classmethod
-    def modulo_odd_even(self):
-        T = []
-        for num in self.nums:
-            start = time.time()
-            if num % 2 == 0:
-                pass
-            else:
-                pass
-            end = time.time()
-            T.append(end - start)
-        return statistics.mean(T)
+    def modulo_odd_even(cls):
+        times = []
+        for num in cls.nums:
+            t = timeit.timeit(lambda: num % 2 == 0, number=1)
+            times.append(t)
+        return statistics.mean(times)
             
     def warmup():
         i = 100_000_000
@@ -46,3 +36,9 @@ if __name__ == "__main__":
     time_from_bitwise = Timed_Odd_Even.bit_odd_even()
     time_from_modulo = Timed_Odd_Even.modulo_odd_even()
     print(f"bitwise: {time_from_bitwise}\nmodulo: {time_from_modulo}")
+    
+    '''
+    Results:
+    bitwise: 3.387022477137342e-07
+    modulo: 1.2299952507019042e-07
+    '''
