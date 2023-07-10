3eee9331630355abaf1e4d3eb8e5f228862ff25a
Diego <lucy@lucy.local>
2023-07-10 16:54:30 -0600

    in place is an example of using slice assignment for inplace insertions

---
+++ in_place.py
@@ -0,0 +1,26 @@
+class In_Place:
+    def __init__(self, nums: list = [1,2,3,4,5], replacements: list = [10,20,30], r_loc : int = 1) -> None:
+        self.nums = nums
+        self.replacements = replacements
+        # self.r_loc = r_loc
+        self.r_loc_start = r_loc
+        self.r_loc_end = r_loc + len(replacements)
+        
+    
+    def slice_assignment(self):
+        id_before_insert = id(self.nums)
+        print (f"{self.nums} is at {id_before_insert}")
+        
+        # nums[start:stop] = replacements 
+        self.nums[self.r_loc_start:self.r_loc_end] = self.replacements
+        
+        id_after_insert = id(self.nums)
+        print (f"{self.nums} is at {id_after_insert}")
+        print(f"since these numbers are the same, it indicates the replacement was done in place")
+        print(f"{id_before_insert}\n{id_after_insert}")
+        
+        
+        
+if __name__ == "__main__":
+    instance = In_Place()
+    instance.slice_assignment()
